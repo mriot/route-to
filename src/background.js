9cebea1d1735d87f4c2918804642d98d6b5b7692
Markus <kremer.markus1996@gmail.com>
2024-06-25 23:34:16 +0200

    improve default location;  open new tab next to source tab

--- src/background.js
+++ src/background.js
@@ -6,9 +6,12 @@ browser.contextMenus.create({
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId !== "routeToSelection") return;
+
     const { startAddress } = await browser.storage.local.get("startAddress");
 
-    browser.tabs.create({
-        url: encodeURI(`https://www.google.com/maps/dir/${startAddress}/${info.selectionText}`),
-    });
+    const url = new URL("https://www.google.com/maps/dir/?api=1");
+    if (startAddress) url.searchParams.set("origin", startAddress);
+    url.searchParams.set("destination", info.selectionText);
+
+    browser.tabs.create({ url: url.href, index: tab.index + 1 });
 });
